9ef237d80fd92c2c7e228ea3db7efac20379d244
hyacinthe <hspanjerspro@gmail.com>
2025-10-07 10:10:30 +0200

    mission et debrief

---
+++ escape_game/mission/page.tsx
@@ -0,0 +1,9 @@
+// app/lobby/page.tsx
+export default function Lobby() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Mission</h1>
+      <p className="mt-2 text-gray-400">Choisis ton rôle et attends les autres joueurs</p>
+    </main>
+  );
+}
